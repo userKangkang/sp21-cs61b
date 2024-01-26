b2de51bca568c1617afa7ef45e671f6f8ae2e50f
userKangkang <hwqqz678@sjtu.edu.cn>
2024-01-26 10:17:25 +0800

    proj1 checkpoint.

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,166 @@
+package deque;
+
+import java.util.Iterator;
+
+public class ArrayDeque<T> implements Iterable<T>{
+    // legal syntax of creating modeled array.
+    private T[] items;
+    private int size;
+    private int head;
+    private int rear;
+    private int capacity;
+
+    public ArrayDeque(){
+        items = (T[]) (new Object[10]);
+        size = 0;
+        head = 0;
+        rear = 0;
+        capacity = 10;
+    }
+
+    public void addFirst(T item){
+        if(isFull()){
+            resize(capacity * 2);
+        }
+        head = (head + 1) % capacity;
+        items[head] = item;
+        size++;
+    }
+
+    public void addLast(T item){
+        if(isFull()){
+            resize(capacity * 2);
+        }
+        items[rear] = item;
+        rear = (rear - 1 + capacity) % capacity;
+        size++;
+    }
+
+    public void resize(int newSize){
+        T[] tmp = (T[]) new Object[newSize];
+        int i;
+        for(i = 0; i < size; i++){
+            tmp[i] = items[(rear + 1 + i)%capacity];
+        }
+        capacity = newSize;
+        head = i - 1;
+        rear = newSize - 1;
+        items = tmp;
+    }
+
+    public boolean isEmpty(){
+        return rear == head;
+    }
+
+    public boolean isFull(){
+        return (rear - head + capacity) % capacity == 1;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        StringBuilder returnSB = new StringBuilder("");
+        int index = head;
+        for(; index != rear; index = (index - 1 + capacity) % capacity){
+            returnSB.append(items[index].toString());
+            returnSB.append(",");
+        }
+        System.out.println(returnSB.toString());
+    }
+
+    public T removeFirst(){
+        if(isEmpty()){
+            return null;
+        }
+        if(size * 4 < capacity && capacity >= 16){
+            resize(size * 2);
+        }
+        T returnValue = items[head];
+        head = (head - 1 + capacity) % capacity;
+        size--;
+        return returnValue;
+    }
+
+    public T removeLast(){
+        if(isEmpty()){
+            return null;
+        }
+        if(size * 4 < capacity && capacity >= 16){
+            resize(size * 2);
+        }
+        T returnValue = items[(rear + 1) % capacity];
+        rear = (rear + 1) % capacity;
+        size--;
+        return returnValue;
+    }
+
+    public T get(int index){
+        if(index >= capacity && index < 0){
+            return null;
+        }
+        return items[(head - index + capacity) % capacity];
+    }
+
+    public class ArrayIterator implements Iterator<T>{
+        int index;
+
+        public ArrayIterator(){
+            index = head;
+        }
+
+        @Override
+        public boolean hasNext(){
+            return index != rear;
+        }
+
+        @Override
+        public T next(){
+            T data = get(index);
+            index = (index + 1) % capacity;
+            return data;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator(){
+        ArrayIterator iter = new ArrayIterator();
+        return (Iterator<T>) iter;
+    }
+
+    public boolean contains(T data){
+        for(int index = head; index != rear; index = (index + 1) % capacity){
+            if(data == items[index]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o){
+            return true;
+        }
+        if(o == null){
+            return false;
+        }
+        if(o.getClass() != this.getClass()){
+            return false;
+        }
+        ArrayDeque<T> other = (ArrayDeque<T>) o;
+        if(other.size() != this.size()){
+            return false;
+        }
+        for(T item : this){
+            if(!other.contains(item)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public T getLast(){
+        return get(size - 1);
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,173 @@
+// declare it in the `deque` package.
+package deque;
+
+import java.util.Iterator;
+
+public class LinkedListDeque<T> implements Iterable<T>{
+    private static class Node<T>{
+        private T data;
+        private Node next;
+        private Node prev;
+
+        Node(T data, Node next, Node prev){
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    Node sentinel;
+    int size;
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new Node(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+
+    public void addFirst(T item) {
+        Node tmp = new Node<>(item, sentinel, sentinel.prev);
+        sentinel.prev = tmp;
+        tmp.prev.next = tmp;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        Node tmp = new Node<>(item, sentinel, sentinel.prev);
+        sentinel.prev.next = tmp;
+        sentinel.prev = tmp;
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public void printDeque(){
+        if(sentinel.next == sentinel){
+            return;
+        }
+        Node p = sentinel.next;
+        String printMessage = "";
+        do{
+            printMessage += p.data.toString() + ",";
+            p = p.next;
+        }while(p == sentinel);
+        System.out.println(printMessage);
+    }
+
+    public T removeFirst(){
+        if(isEmpty()){
+            return null;
+        }
+        T returnValue = (T)sentinel.next.data;
+        Node p = sentinel.next;
+        p.next.prev = sentinel;
+        sentinel.next = p.next;
+        size -= 1;
+        return returnValue;
+    }
+
+    public T removeLast(){
+        if(isEmpty()){
+            return null;
+        }
+        T returnValue = (T)sentinel.prev.data;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        size -= 1;
+        return returnValue;
+    }
+
+    public T get(int index) {
+        if(isEmpty()){
+            return null;
+        }
+        Node p = sentinel;
+        for(int i = 0; i < index; i++){
+            p = p.next;
+        }
+        return (T)p.data;
+    }
+
+    public T getRecursive(int index){
+        return recursiveHelper(index, sentinel.next);
+    }
+
+    public T recursiveHelper(int index, Node p){
+        if(p == sentinel){
+            return null;
+        }
+        if(index == 0){
+            return (T)p.data;
+        }
+        return recursiveHelper(index - 1, p.next);
+    }
+
+    public class LinkIterator implements Iterator<T>{
+        Node p;
+
+        public LinkIterator(Node node){
+            p = node;
+        }
+
+        @Override
+        public boolean hasNext(){
+            return p != null;
+        }
+
+        @Override
+        public T next(){
+            T data = (T)p.data;
+            p = p.next;
+            return data;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator(){
+        LinkIterator iter = new LinkIterator(sentinel);
+        return (Iterator<T>) iter;
+    }
+
+    public boolean contains(T item){
+        Node p = sentinel;
+        do{
+            if(p.data == item){
+                return true;
+            }
+            p = p.next;
+        }while(p != sentinel);
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o){
+            return true;
+        }
+        if(o == null){
+            return false;
+        }
+        if(o.getClass() != this.getClass()){
+            return false;
+        }
+        LinkedListDeque<T> other = (LinkedListDeque<T>) o;
+        if(other.size() != size){
+            return false;
+        }
+        for(T item : this){
+            if(!other.contains(item)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
